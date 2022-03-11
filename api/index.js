61139a49a4f9be8cdb10bbec0a5f6300671f20e0
Athul Raj <athulraj786@gmail.com>
2022-03-11 21:09:16 +0530

    new

--- api/index.js
+++ api/index.js
@@ -5,10 +5,13 @@ const mongoose = require('mongoose');
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/users');
 const postRoute = require('./routes/posts');
+const categoryRoute = require('./routes/categories');
+const multer = require("multer");
 
 dotenv.config();
 app.use(express.json());
 
+//mongo
 mongoose.connect(process.env.MONGO_URL,{
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -16,9 +19,26 @@ mongoose.connect(process.env.MONGO_URL,{
 .then(console.log("Connected to MONGODB"))
 .catch((err)=>console.log(err));
 
+//images file storage
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, req.body.name);
+    },
+});
+
+const upload = multer({ storage: storage});
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("File has been Uploaded");
+});
+
+//routes
 app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
 app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
 
 
 app.listen('3000',()=>{
