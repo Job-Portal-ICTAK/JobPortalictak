b2c83e198dca9b69a45bcce32137be83ecb55987
Athul Raj <athulraj786@gmail.com>
2022-03-12 13:29:49 +0530

    new

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -9,7 +9,6 @@ export default function Register() {
   const [password, setPassword] = useState("");
   const [error, setError] = useState(false);
 
-<<<<<<< HEAD
   //handle the inputs
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -23,7 +22,6 @@ export default function Register() {
       setError(true);
     }
    }
-=======
   const handleSubmit = async (e) => {
     e.preventDefault();
     setError(false);
@@ -37,13 +35,11 @@ export default function Register() {
     }
   };
 
->>>>>>> ac399b7e2dcbbd4944518eacd4ce9cae1909c156
     return (
         <div className="register">
       <span className="registerTitle">Register</span>
       <form className="registerForm" onSubmit={handleSubmit}>
         <label>Username</label>
-<<<<<<< HEAD
         <input className="registerInput" type="text"
         placeholder="Enter your username..."
         onChange={e=>setUsername(e.target.value)} 
@@ -58,13 +54,11 @@ export default function Register() {
         placeholder="Enter your password..."
         onChange={e=>setPassword(e.target.value)} 
         />
-=======
         <input className="registerInput" type="text" placeholder="Enter your username..." onChange={e=>setUsername(e.target.value)}/>
         <label>Email</label>
         <input className="registerInput" type="text" placeholder="Enter your email..." onChange={e=>setEmail(e.target.value)}/>
         <label>Password</label>
         <input className="registerInput" type="password" placeholder="Enter your password..." onChange={e=>setPassword(e.target.value)}/>
->>>>>>> ac399b7e2dcbbd4944518eacd4ce9cae1909c156
         <button className="registerButton" type="submit">Register</button>
       </form>
         <button className="registerLoginButton">
@@ -72,11 +66,8 @@ export default function Register() {
           LOGIN
         </Link>
         </button>
-<<<<<<< HEAD
         {error && <span style={{color:"red"}} >Something went Wrong!</span>}
-=======
         {error && <span style={{color:"red", marginTop:"10px"}}>Something went wrong!</span>}
->>>>>>> ac399b7e2dcbbd4944518eacd4ce9cae1909c156
     </div>
     )
 }
