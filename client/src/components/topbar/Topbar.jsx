fe3c764befba580da090e72ecfaf9b448b862bb7
Femi Francis <ffmief@gmail.com>
2022-03-12 14:01:29 +0530

    Integration4

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -4,7 +4,11 @@ import { Context } from "../../context/Context";
 import "./topbar.css";
 
 export default function Topbar() {
-  const {user} = useContext(Context);
+  const {user, dispatch} = useContext(Context);
+
+  const handleLogout = () => {
+   dispatch({type: "LOGOUT"});
+   }
   return (
     <div className="top">
       <div className="topLeft">
@@ -27,7 +31,7 @@ export default function Topbar() {
               WRITE
             </Link>
           </li>
-          {user && <li className="topListItem">LOGOUT</li>}
+          {user && <li className="topListItem" onClick={handleLogout}>LOGOUT</li>}
         </ul>
       </div>
       <div className="topRight">
